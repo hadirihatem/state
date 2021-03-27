12964f93a616f166a6abb073ec69020c1fc8f56b
hatem <hatemhadiri9@gmail.com>
2021-03-27 04:05:51 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+    constructor(props) {
+      super(props)
+    this.state = {
+      Person : {fullname:"Hadiri Hatem", bio:"TO BUSY!", profession:"flight despatcher", image:"dont.jpg"},
+      show : true,
+     
+    
+    };
+  }
+    render(){
+      
+      return (
+      <div className="App-header">
+        {
+          this.state.show?
+        <div>
+          <p>{this.state.Person.fullname}</p>
+          <p>{this.state.Person.bio}</p>
+          <div><img src={this.state.Person.image} alt={"move"}style={{width:200}}></img></div>
+          <p>{this.state.Person.profession}</p>
+        </div>
+        : "you are welcome"
+        }
+        <button onClick={()=>this.setState({...this.state,show:!this.state.show})}>clickme</button>
+      </div>)
+    }
+     
+  }
 
-export default App;
+
+
+
